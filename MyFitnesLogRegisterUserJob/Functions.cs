6e47e37f01a778a2c2249389fc2b22ef6f95752b
Manoj Choudhari <manoj_choudhari@outlook.com>
2020-01-11 16:29:13 +0100

    Added web job

---
+++ MyFitnesLogRegisterUserJob/Functions.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.Azure.WebJobs;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MyFitnesLogRegisterUserJob
+{
+    public class Functions
+    {
+        public static void ProcessQueueMessage([QueueTrigger("my-queue")] string message, ILogger logger)
+        {
+            logger.LogInformation("START of Execution of your Web Job");
+            logger.LogInformation(message);
+            logger.LogInformation("END of Execution of your Web Job");
+        }
+    }
+}
---
+++ MyFitnesLogRegisterUserJob/Program.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using System;
+
+namespace MyFitnesLogRegisterUserJob
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var builder = new HostBuilder();
+            builder.ConfigureWebJobs(b =>
+            {
+                b.AddAzureStorageCoreServices();
+                b.AddAzureStorage();
+            });
+            builder.ConfigureLogging((loggingBuilder) =>
+            {
+                loggingBuilder.AddConsole();
+            });
+
+            var host = builder.Build();
+            using (host)
+            {
+                host.Run();
+            }
+        }
+    }
+}
